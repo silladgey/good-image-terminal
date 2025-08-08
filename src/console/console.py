b015ab957e90c0b483158dc3a05c102e8a70b289
Philip Van Den Akker <64905829+osiris32-and-a-half@users.noreply.github.com>
2025-08-08 14:45:11 +0100

    Added docstrings to console.py

--- src/console/console.py
+++ src/console/console.py
@@ -1,9 +1,17 @@
 class Console:
-    def run_str(self, command_str: str):
+    """Console manages a custom command environment."""
+
+    def run_str(self, command_str: str) -> bool:
+        """Parse and then run the given command.
+
+        :param command_str: String of command to be executed
+        :return: success of command execution
+        """
         command_str = command_str.strip()
         print(command_str)
+        return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_console = Console()
     test_console.run_str(input())
