29509e12fb34a38f6b3876f9b69d400b09305782
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-09 15:30:26 +0100

    Refactored helps argument parsing to use a match case

--- src/commands/help.py
+++ src/commands/help.py
@@ -29,18 +29,21 @@ class Help(BaseCommand):
         @return True if command was executed successfully.
         """
         page = 1
-        if len(args) == 0:
-            terminal.output_info("Available commands: ")
-            terminal.output_info(", ".join(sorted(commands.all_commands.keys())))
-            terminal.output_info("for more information on a command use `help command`.")
-            return True
-        if len(args) >= 3:
-            terminal.output_error("too many arguments.")
-            return False
-        if len(args) == 2:
-            if not args[1].isdigit():
-                terminal.output_error("second argument must be an integer.")
-            page = args[1]
+        match len(args):
+            case 0:
+                terminal.output_info("Available commands: ")
+                terminal.output_info(", ".join(sorted(commands.all_commands.keys())))
+                terminal.output_info("for more information on a command use `help command`.")
+                return True
+            case 1:
+                page = 1
+            case 2:
+                if not args[1].isdigit():
+                    terminal.output_error("second argument must be an integer.")
+                page = args[1]
+            case _:
+                terminal.output_error("too many arguments.")
+                return False
 
         if args[0] not in commands.all_commands:
             terminal.output_error(f"`{args[0]}` is an Unknown command.")
