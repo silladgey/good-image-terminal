be8863c494cd9d73f0688491692c264544c1d4eb
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-09 13:07:31 +0100

    Added stubs for outputting information
    They allow for the setting of which colour each type of info will be outputted in

--- src/terminal.py
+++ src/terminal.py
@@ -1,8 +1,12 @@
 class Terminal:
     """Terminal manages a custom command environment.
 
-    @author
+    @author Philip
     """
+
+    info_colour = (255, 255, 255)
+    success_colour = (0, 255, 0)
+    error_colour = (255, 0, 0)
 
     def run_str(self, command_str: str) -> bool:
         """Parse and then run the given command.
@@ -14,6 +18,33 @@ class Terminal:
         command_str = command_str.strip()
         print(command_str)
         return True
+
+    def output_info(self, output: str) -> None:
+        """Output the given input to the display with `info_colour`.
+
+        @author Philip
+        @param output: Text to be printed
+        @return None
+        """
+        print(output)
+
+    def output_success(self, output: str) -> None:
+        """Output the given input to the display with `success_colour`.
+
+        @author Philip
+        @param output: Text to be printed
+        @return None
+        """
+        print(output)
+
+    def output_error(self, output: str) -> None:
+        """Output the given input to the display with `error_colour`.
+
+        @author Philip
+        @param output: Text to be printed
+        @return None
+        """
+        print(output)
 
 
 if __name__ == "__main__":
