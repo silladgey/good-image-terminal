93a0fe29ff22ac8d46cc246b9ad55ce4711e5bc8
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-09 15:08:40 +0100

    Added functionality to the help command

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,3 +1,4 @@
+from src import commands
 from src.commands import BaseCommand
 from src.terminal import Terminal
 
@@ -27,5 +28,32 @@ class Help(BaseCommand):
         @param args: Arguments to be passed to the command.
         @return True if command was executed successfully.
         """
-        terminal.output_info(f"help command ran with given arguments: {args}")
+        page = 1
+        if len(args) == 0:
+            terminal.output_info("Available commands: ")
+            terminal.output_info(", ".join(sorted(commands.all_commands.keys())))
+            terminal.output_info("for more information on a command use `help command`.")
+            return True
+        if len(args) >= 3:
+            terminal.output_error("too many arguments.")
+            return False
+        if len(args) == 2:
+            if not args[1].isdigit():
+                terminal.output_error("second argument must be an integer.")
+            page = args[1]
+
+        if args[0] not in commands.all_commands:
+            terminal.output_error(f"`{args[0]}` is an Unknown command.")
+            terminal.output_error("use `help` to see a list of available commands")
+            return False
+
+        command = commands.all_commands[args[0]]
+
+        if page > len(command.help_pages):
+            terminal.output_error(f"`{args[0]}` is not a valid page.")
+            return False
+
+        terminal.output_info(f"help for `{args[0]}`\t\t page: {page}/{len(command.help_pages)}")
+        terminal.output_info(command.help_pages[page - 1])
+
         return True
