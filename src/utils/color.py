69d87a6ec955b102ef228067de8905ef42f2df9f
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-10 16:08:19 +0100

    Added return types to color

--- src/utils/color.py
+++ src/utils/color.py
@@ -7,17 +7,18 @@ class Color:
 
     Has various conversion and output methods.
     """
+
     r: int
     g: int
     b: int
     a: int = 255
 
     @property
-    def rgb(self):
+    def rgb(self) -> tuple[int, int, int]:
         """Output as RGB tuple."""
         return self.r, self.g, self.b
 
     @property
-    def rgba(self):
+    def rgba(self) -> tuple[int, int, int, int]:
         """Output as RGBA tuple."""
         return self.r, self.g, self.b, self.a
