78db669b29fc53ffde3ddc40921a4a89c4aea59c
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-10 15:58:52 +0100

    added color class

---
+++ src/Utils/color.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Color:
+    r: int
+    g: int
+    b: int
+    a: int = 255
+
+    @property
+    def rgb(self):
+        return self.r, self.g, self.b
+
+    @property
+    def rgba(self):
+        return self.r, self.g, self.b, self.a
