e85a979ad39c28bba175b88ec34b26dab165ceb8
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-10 16:07:10 +0100

    Added Docstrings to color

--- src/utils/color.py
+++ src/utils/color.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Color:
+    """Color class.
+
+    Has various conversion and output methods.
+    """
     r: int
     g: int
     b: int
@@ -10,9 +14,10 @@ class Color:
 
     @property
     def rgb(self):
+        """Output as RGB tuple."""
         return self.r, self.g, self.b
 
     @property
     def rgba(self):
+        """Output as RGBA tuple."""
         return self.r, self.g, self.b, self.a
-
