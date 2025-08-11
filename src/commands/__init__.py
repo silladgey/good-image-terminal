5ddfbc4b884b696e92109571668f81ec4e9e3216
Richard Szilagyi <szprichard@proton.me>
2025-08-11 22:50:31 +0100

    Fix linting errors

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,8 +1,8 @@
+from commands.background import Background
 from commands.base_command import BaseCommand
 from commands.help import Help
 from commands.load_image import LoadImage
 from commands.ping import Ping
-from commands.background import Background
 
 all_commands: dict[str, BaseCommand] = {
     Ping.name: Ping(),
--- src/commands/background.py
+++ src/commands/background.py
@@ -16,6 +16,11 @@ class Background(BaseCommand):
     @author Julien
     """
 
+    RGB_COMPONENTS = 3
+    MAX_RGB_VALUE = 255
+    MIN_RGB_VALUE = 0
+    BRIGHTNESS_THRESHOLD = 0.5
+
     name: str = "bg"
     help_pages: tuple[str, ...] = (
         """background is a command that changes the background.
@@ -27,7 +32,7 @@ class Background(BaseCommand):
     )
 
     def __call__(self, terminal: "Terminal", *args: str) -> bool:
-        """Change the background color of the web page
+        """Change the background color of the web page.
 
         :param terminal: The terminal instance.
         :param args: Arguments to be passed to the command.
@@ -35,29 +40,25 @@ class Background(BaseCommand):
 
         @author Julien
         """
-
         # filtering user input #
         if not args:
             terminal.output_error(
-                "You need to provide the color of the background to change it"
+                "You need to provide the color of the background to change it",
             )
             return False
 
         if not args[0].startswith("rgb("):
             terminal.output_error(
-                "wrong argument expected format: bg rgb(number, number, number)"
+                "wrong argument expected format: bg rgb(number, number, number)",
             )
             return False
 
-        # Number of expected RGB components
-        RGB_COMPONENTS = 3
-        if len(args) < RGB_COMPONENTS:
+        if len(args) < self.RGB_COMPONENTS:
             terminal.output_error(
-                "wrong argument expected format: bg rgb(number, number, number)"
+                "wrong argument expected format: bg rgb(number, number, number)",
             )
             return False
         list_args = [args[0].replace("rgb(", ""), args[1], args[2].replace(")", "")]
-
 
         rgb = []
         for index in range(3):
@@ -66,44 +67,56 @@ class Background(BaseCommand):
 
             if not value.isdigit():
                 terminal.output_error(
-                    f"wrong argument expected format: bg rgb(number, number, number): {value}"
+                    f"wrong argument expected format: bg rgb(number, number, number): {value}",
                 )
                 return False
-            
+
             value = int(value)
 
-            if 0 > value > 255:
-                terminal.output_error("rgb value too high or to low, max value is 255, min is 0")
+            if value < self.MIN_RGB_VALUE or value > self.MAX_RGB_VALUE:
+                terminal.output_error(
+                    f"rgb value too high or too low, max value is {self.MAX_RGB_VALUE}, min is {self.MIN_RGB_VALUE}",
+                )
                 return False
-            
+
             rgb.append(value)
         # filtering user input #
 
-
         color = Color(rgb[0], rgb[1], rgb[2])
 
-
         # changes the background color of the terminal
-        terminal.terminal_display["style"].setProperty("--terminal-background-color", f"rgb{color.rgb}")
-        text_color = Color(255, 255, 255) if color.hsv[2] < 0.5 else Color(0, 0, 0)
+        terminal.terminal_display["style"].setProperty(
+            "--terminal-background-color",
+            f"rgb{color.rgb}",
+        )
+
+        text_color = (
+            Color(255, 255, 255)
+            if color.hsv[2] < self.BRIGHTNESS_THRESHOLD
+            else Color(0, 0, 0)
+        )
         terminal.info_colour = text_color
         # changes the text color of all the users input
-        terminal.terminal_display["style"].setProperty("--terminal-output-color", f"rgb{text_color.rgb}")
-
+        terminal.terminal_display["style"].setProperty(
+            "--terminal-output-color",
+            f"rgb{text_color.rgb}",
+        )
 
         # replace the color of all the outputs in the terminal history #
-        old_elements = terminal.terminal_display.history._elements.copy()
+        old_elements = terminal.terminal_display.history.get_elements().copy()
         terminal.terminal_display.history.clear_history()
         for element in old_elements:
 
             if element.class_name == "terminal-output":
-                modified_output = _TerminalOutput(element.text, "rgb" + str(text_color.rgb))
+                modified_output = _TerminalOutput(
+                    element.text,
+                    "rgb" + str(text_color.rgb),
+                )
                 terminal.terminal_display.history.add_history(modified_output)
 
             else:
                 terminal.terminal_display.history.add_history(element)
         # replace the color of all the outputs in the terminal history #
-
 
         terminal.output_info("background-color succesfully changed")
         return True
