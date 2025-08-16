532f8ee6b4450de4880c277706555ebd01d74fb0
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-16 23:34:31 +0100

    Used fg from inputted options instead of terminal

--- src/commands/draw_rectangle.py
+++ src/commands/draw_rectangle.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from commands.base_command import BaseCommand
+from utils.color import Color
 
 if TYPE_CHECKING:
     from terminal import Terminal
@@ -25,7 +26,7 @@ class DrawRectangle(BaseCommand):
     )
     known_options = ("fg", "bg")
 
-    def __call__(self, terminal: "Terminal", *args: str, **_options: str) -> bool:
+    def __call__(self, terminal: "Terminal", *args: str, **options: str | Color) -> bool:
         """Draw rectangle command.
 
         :param terminal: The terminal instance.
@@ -50,11 +51,11 @@ class DrawRectangle(BaseCommand):
             terminal.output_error("Invalid size.")
             return False
         w, h = int(args[2]), int(args[3])
-        terminal.image.fill_rect(x, y, w, h, terminal.foreground_color)
+        terminal.image.fill_rect(x, y, w, h, options["fg"])
         terminal.output_info(f"rectangle at {x}x{y} size {w}x{h} filled with rgb{terminal.foreground_color.rgb}")
         return True
 
-    def predict_args(self, _terminal: "Terminal", *args: str, **_options: str) -> str | None:
+    def predict_args(self, _terminal: "Terminal", *args: str, **_options: str | Color) -> str | None:
         """Argument predictor."""
         result = ""
         match len(args):
