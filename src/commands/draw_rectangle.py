974c7854361c768b01ca968b43834496a4f1874f
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-16 23:08:12 +0100

    Changed draw_rectangle to use the new fg system

--- src/commands/draw_rectangle.py
+++ src/commands/draw_rectangle.py
@@ -1,10 +1,11 @@
 from typing import TYPE_CHECKING
 
 from commands.base_command import BaseCommand
-from utils.color import Color, colors
 
 if TYPE_CHECKING:
     from terminal import Terminal
+
+REQUIRED_NUMBER_ARGS = 4
 
 
 class DrawRectangle(BaseCommand):
@@ -16,15 +17,13 @@ class DrawRectangle(BaseCommand):
     name: str = "draw_rectangle"
     help_pages: tuple[str, ...] = (
         """
-        Usage: draw_rectangle x y width height color
-        or: draw_rectangle x y width height r g b
+        Usage: draw_rectangle x y width height
 
         arguments x,y: coordinate numbers
         arguments width,height: width and height of the rectangle
-        argument color: color name
-        arguments r,g,b: red,green,blue numbers
         """,
     )
+    known_options = ("fg", "bg")
 
     def __call__(self, terminal: "Terminal", *args: str, **_options: str) -> bool:
         """Draw rectangle command.
@@ -36,19 +35,10 @@ class DrawRectangle(BaseCommand):
 
         @author Mira
         """
-        if len(args) == 5:  # noqa: PLR2004
-            if args[4] not in colors:
-                terminal.output_error("Invalid color name.")
-                return False
-            col = colors[args[4]]
-        elif len(args) == 7:  # noqa: PLR2004
-            if not all([a.isdigit() and 0 <= int(a) < 256 for a in args[4:]]):  # noqa: PLR2004, C419
-                terminal.output_error("Wrong color, please input `r g b` as numbers 0-255.")
-                return False
-            col = Color(int(args[4]), int(args[5]), int(args[6]))
-        else:
+        if len(args) != REQUIRED_NUMBER_ARGS:
             terminal.output_error("Bad amount of arguments, see help for options")
             return False
+
         size = terminal.image.img.size
         if not (
             args[0].isdigit() and args[1].isdigit() and 0 <= int(args[0]) < size[0] and 0 <= int(args[1]) < size[1]
@@ -60,32 +50,20 @@ class DrawRectangle(BaseCommand):
             terminal.output_error("Invalid size.")
             return False
         w, h = int(args[2]), int(args[3])
-        terminal.image.fill_rect(x, y, w, h, col)
-        terminal.output_info(f"rectangle at {x}x{y} size {w}x{h} filled with rgb{col.rgb}")
+        terminal.image.fill_rect(x, y, w, h, terminal.foreground_color)
+        terminal.output_info(f"rectangle at {x}x{y} size {w}x{h} filled with rgb{terminal.foreground_color.rgb}")
         return True
 
-    def predict_args(self, _terminal: "Terminal", *args: str, **_options: str) -> str | None:  # noqa: C901
+    def predict_args(self, _terminal: "Terminal", *args: str, **_options: str) -> str | None:
         """Argument predictor."""
         result = ""
         match len(args):
             case 0:
-                result = " x y width height color"
+                result = " x"
             case 1:
-                result = " y width height color"
+                result = " y"
             case 2:
-                result = " width height color"
+                result = " width"
             case 3:
-                result = " height color"
-            case 4:
-                result = " color"
-            case 5:
-                for col in colors:
-                    if col.startswith(args[2]):
-                        result = col
-                if args[4].isdigit():
-                    result = " g b"
-            case 6:
-                result = " b"
-            case _:
-                pass
+                result = " height"
         return result
