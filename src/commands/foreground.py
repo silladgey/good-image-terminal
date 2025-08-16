287432dc182537b66413178487bcae2b7fc69c0a
Philip Van Den Akker <64905829+Osiris32-and-a-half@users.noreply.github.com>
2025-08-16 23:42:05 +0100

    Fixed fg prediction to use foreground color

--- src/commands/foreground.py
+++ src/commands/foreground.py
@@ -56,12 +56,12 @@ class Foreground(BaseCommand):
 
         match len(args):
             case 0:
-                return " " + str(terminal.background_color.r)
+                return " " + str(terminal.foreground_color.r)
             case 1:
-                return " " + str(terminal.background_color.g)
+                return " " + str(terminal.foreground_color.g)
             case 2:
-                return " " + str(terminal.background_color.b)
+                return " " + str(terminal.foreground_color.b)
             case 3:
-                return " " + str(terminal.background_color.a)
+                return " " + str(terminal.foreground_color.a)
 
         return None
